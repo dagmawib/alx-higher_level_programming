09bcbff317d38419dc5da6e797e94e5ea230fc01
Dagib <dagmawibelayneh14@gmail.com>
2023-04-10 19:25:49 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const firstArg = process.argv[2];
-if(firstArg = undefined){
-    console.log("No arguments");
-}
-else{
-    console.log(firstArg);
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const argConvert = parseInt(process.argv[2]);
-if (isNaN(argConvert)){
-    console.log('Not a number');
-}
-else{
-    console.log('My number: ' + argConvert);
-}
